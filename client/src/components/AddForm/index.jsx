212be9989f9ae7c9516af9939cab09f78d819bad
Cory Benton <corybenton@gmail.com>
2023-10-29 11:13:10 -0400

    add form started

--- client/src/components/AddForm/index.jsx
+++ client/src/components/AddForm/index.jsx
@@ -1,6 +1,110 @@
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+
+import List from '../List';
+//import { ADD_MEMBER } from '../../utilities/mutations';
+
 const AddForm = () => {
-    return(
-        <p>Add</p>
+    const [formState, setFormState] = useState({
+        memberName: '',
+        email: '',
+        memberYears: '',
+        address: '',
+        agesOfKids: '',
+        payer: '',
+        notes: '',
+    });
+
+    //const [addMember, { error }] = useMutation(ADD_MEMBER);
+
+    const handleInputChange = (e) => {
+        const { inputType, inputValue } = e.target;
+
+        setFormState({ ...formState, [inputType]: inputValue })
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const { data } = addMember({
+                variables: { ...formState },
+            });
+
+            setFormState({
+                memberName: '',
+                email: '',
+                memberYears: '',
+                address: '',
+                agesOfKids: '',
+                payer: '',
+                notess: '',
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='memberName'>Name(s):</label>
+                <input
+                    value={formState.memberName}
+                    name='memberName'
+                    onChange={handleInputChange}
+                    type='text'
+                    placeholder='First Last'
+                />
+                <label htmlFor='email'>Email(s):</label>
+                <input
+                    value={formState.email}
+                    name='email'
+                    onChange={handleInputChange}
+                    type='text'
+                    placeholder='name@email.com'
+                />
+                <label htmlFor='memberYears'>Member In:</label>
+                <input
+                    value={formState.memberYears}
+                    name='memberYears'
+                    onChange={handleInputChange}
+                    type='text'
+                    placeholder='2023'
+                />
+                <label htmlFor='address'>Address:</label>
+                <input
+                    value={formState.address}
+                    name='address'
+                    onChange={handleInputChange}
+                    type='text'
+                    placeholder='123 Main St'
+                />
+                <label htmlFor='agesOfKids'>Kid(s) Age:</label>
+                <input
+                    value={formState.agesOfKids}
+                    name='agesOfKids'
+                    onChange={handleInputChange}
+                    type='text'
+                    placeholder='Age'
+                />
+                <label htmlFor='payer'>Payer:</label>
+                <input
+                    value={formState.payer}
+                    name='payer'
+                    onChange={handleInputChange}
+                    type='checkbox'
+                />
+                <label htmlFor='notes'>Notes:</label>
+                <input
+                    value={formState.notes}
+                    name='notes'
+                    onChange={handleInputChange}
+                    type='text'
+                    placeholder='Additional info'
+                />
+            </form>
+        </div>
     );
 };
 
